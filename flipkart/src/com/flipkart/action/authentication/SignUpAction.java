3e7d115469acbcce754ddad0f8f5e0fe63e601a1
aamirshaad.alam@gmail.com <aamirshaad.alam@gmail.com@9734beb7-efc3-7ff8-bd58-a83438845b4c>
2013-03-29 19:23:05 +0000

    sign up

---
+++ flipkart/src/com/flipkart/action/authentication/SignUpAction.java
@@ -0,0 +1,43 @@
+package com.flipkart.action.authentication;
+
+import com.flipkart.model.authentication.SignUpModel;
+import com.flipkart.util.MyLog;
+import com.flipkart.util.MyUtilityFunctions;
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
+public class SignUpAction extends ActionSupport {
+
+	String emailAddress;
+	String password;
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String createAccount() {
+
+		if (SignUpModel.checkUser(emailAddress)) {
+			addActionError("An account already exists with the same email address."
+					+ " Login or create an account with another email address.");
+			MyLog.log("An account already exists with the same email address"
+					+ " Login or create an account with another email address.");
+			return ERROR;
+		} else
+			SignUpModel.createAccount(emailAddress, MyUtilityFunctions.generateMD5(password));
+		return SUCCESS;
+
+	}
+}
