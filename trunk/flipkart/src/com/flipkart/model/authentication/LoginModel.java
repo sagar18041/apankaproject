47f4164f9212ef929f32cbfe55e538cafe28f212
aamirshaad.alam@gmail.com <aamirshaad.alam@gmail.com@9734beb7-efc3-7ff8-bd58-a83438845b4c>
2013-03-29 20:46:08 +0000

    login

---
+++ trunk/flipkart/src/com/flipkart/model/authentication/LoginModel.java
@@ -0,0 +1,60 @@
+package com.flipkart.model.authentication;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.flipkart.util.DbConnection;
+
+public class LoginModel {
+
+	static PreparedStatement ps = null;
+	static ResultSet rs = null;
+	static String sqlQuery = "";
+	static Connection conn = null;
+
+	public static boolean isAuthentic(String emailAddress, String password) {
+
+		sqlQuery = "SELECT * FROM flipkart.flipkart_userinfo where "
+				+ "emailAddress = ? and " + "password = ?;";
+
+		conn = DbConnection.getConnection();
+
+		try {
+			ps = conn.prepareStatement(sqlQuery);
+			ps.setString(1, emailAddress);
+			ps.setString(2, password);
+			rs = ps.executeQuery();
+			if (rs.next())
+				return true;
+			else
+				return false;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return false;
+
+	}
+
+	public static int getUserID(String emailAddress) {
+
+		sqlQuery = "SELECT userID FROM flipkart.flipkart_userinfo"
+				+ " where emailAddress = ?;";
+		conn = DbConnection.getConnection();
+		try {
+			ps = conn.prepareStatement(sqlQuery);
+			ps.setString(1, emailAddress);
+			rs = ps.executeQuery();
+			if (rs.next())
+				return rs.getInt(1);
+			else
+				return 0;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+}
