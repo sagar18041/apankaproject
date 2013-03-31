e73788eae64767fdbc408d0bf1def17514b2e63d
guptaneha38@gmail.com <guptaneha38@gmail.com@9734beb7-efc3-7ff8-bd58-a83438845b4c>
2013-03-31 07:26:28 +0000

    Changed for SearchList
    Done by 
    Neha

--- flipkart/src/com/flipkart/action/authentication/HomeAction.java
+++ flipkart/src/com/flipkart/action/authentication/HomeAction.java
@@ -4,6 +4,8 @@
 package com.flipkart.action.authentication;
 
 import com.flipkart.model.authentication.HomeModel;
+import com.flipkart.model.authentication.SearchList;
+import com.flipkart.model.authentication.SearchListModel;
 import com.flipkart.util.MyLog;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -15,8 +17,29 @@ import java.util.*;
  */
 public class HomeAction extends ActionSupport {
 	private ArrayList<String> categoryList  = new ArrayList<String>();
+	ArrayList<SearchList> sl = new ArrayList<SearchList>();
 	String searchBy;
+	String autoCompleteList;
 	
+	
+	
+
+	public String getAutoCompleteList() {
+		return autoCompleteList;
+	}
+
+	public void setAutoCompleteList(String autoCompleteList) {
+		this.autoCompleteList = autoCompleteList;
+	}
+
+	public ArrayList<SearchList> getSl() {
+		return sl;
+	}
+
+	public void setSl(ArrayList<SearchList> sl) {
+		this.sl = sl;
+	}
+
 	public String getSearchBy() {
 		return searchBy;
 	}
@@ -38,6 +61,21 @@ public class HomeAction extends ActionSupport {
 		//get all categories from category table to populate the search box
 		HomeModel hm = new HomeModel();
 		categoryList = hm.getCategoryList();
+		
+		/*
+		 * For searchList
+		 */
+		sl = SearchListModel.fetchList();
+		System.out.println("SearchList size " + sl.size());
+		autoCompleteList = "[\"";
+		for(int i = 0 ; i < sl.size() ; i++){
+			autoCompleteList = autoCompleteList + sl.get(i).fieldValue + "\",\"";
+		}
+		autoCompleteList = autoCompleteList + "\"]";
+	//	autoCompleteList = new String[sl.size()];
+		//autoCompleteList =sl.toArray();
+		//System.out.println("Debug array = " + autoCompleteList.length);
+		
 		//System.out.println(searchBy);
 		return SUCCESS;
 	}
