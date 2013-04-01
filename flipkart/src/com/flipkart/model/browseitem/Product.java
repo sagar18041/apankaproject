41ca0f4c197b1190da3bf6b29085d1ce047edb69
rahul.vit09@gmail.com <rahul.vit09@gmail.com@9734beb7-efc3-7ff8-bd58-a83438845b4c>
2013-04-01 20:32:05 +0000

    product related

---
+++ flipkart/src/com/flipkart/model/browseitem/Product.java
@@ -0,0 +1,27 @@
+package com.flipkart.model.browseitem;
+
+public class Product {
+
+	private Integer itemID;
+	private String itemName;
+	private Integer productID;
+	
+	public Integer getItemID() {
+		return itemID;
+	}
+	public void setItemID(Integer itemID) {
+		this.itemID = itemID;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public Integer getProductID() {
+		return productID;
+	}
+	public void setProductID(Integer productID) {
+		this.productID = productID;
+	}
+}
