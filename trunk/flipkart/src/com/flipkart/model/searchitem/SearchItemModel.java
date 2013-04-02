920c0a2ccafe160811230a0a7ac37b16676026ea
guptaneha38@gmail.com <guptaneha38@gmail.com@9734beb7-efc3-7ff8-bd58-a83438845b4c>
2013-04-02 20:24:43 +0000

    working for search
    Done by -
    Neha

--- trunk/flipkart/src/com/flipkart/model/searchitem/SearchItemModel.java
+++ trunk/flipkart/src/com/flipkart/model/searchitem/SearchItemModel.java
@@ -1,5 +1,19 @@
 package com.flipkart.model.searchitem;
 
-public class SearchItemModel {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+/**
+ * @author neha
+ * This method is used to fetch the items for the search text and category
+ * @return returns the arraylist of the item list
+ */
+public class SearchItemModel {
+	static PreparedStatement ps=null;
+	static ResultSet rs=null;
+	static String sqlQuery="";
+	static Connection conn = null;
+	
+	
 }
