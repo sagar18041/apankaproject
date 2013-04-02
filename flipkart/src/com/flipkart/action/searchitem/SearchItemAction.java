c3e8dd012f8f4bc24d398dc6881bb8e35404c03a
guptaneha38@gmail.com <guptaneha38@gmail.com@9734beb7-efc3-7ff8-bd58-a83438845b4c>
2013-04-02 21:47:59 +0000

    show categories on search page
    Done by 
    Neha

--- flipkart/src/com/flipkart/action/searchitem/SearchItemAction.java
+++ flipkart/src/com/flipkart/action/searchitem/SearchItemAction.java
@@ -1,12 +1,27 @@
 package com.flipkart.action.searchitem;
 
+import java.util.ArrayList;
+
+import com.flipkart.model.searchitem.SearchCategory;
+import com.flipkart.model.searchitem.SearchCategoryModel;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class SearchItemAction extends ActionSupport{
 	public String searchText;
 	public String categorySel;
+	ArrayList<SearchCategory> searchCatg = new ArrayList<SearchCategory>();
 	
 	
+	public ArrayList<SearchCategory> getSearchCatg() {
+		return searchCatg;
+	}
+
+
+	public void setSearchCatg(ArrayList<SearchCategory> searchCatg) {
+		this.searchCatg = searchCatg;
+	}
+
+
 	public String getSearchText() {
 		return searchText;
 	}
@@ -35,6 +50,11 @@ public class SearchItemAction extends ActionSupport{
 		System.out.println("in search action.....");
 		System.out.println("Category = " + getCategorySel());
 		System.out.println("text = " + getSearchText());
+		
+		SearchCategoryModel scm = new SearchCategoryModel();
+		searchCatg = scm.fetchForAllCat(getSearchText());
+		
+		System.out.println("Items List = " + searchCatg.size());
 		return SUCCESS;
 	}
 }
