1253984c8eb068b1b3388ce40df6fc00565c8074
dipanzan.ddb <dipanzan.ddb@9734beb7-efc3-7ff8-bd58-a83438845b4c>
2013-04-07 12:40:04 +0000

    +Deadman added changes

---
+++ flipkart/src/com/flipkart/model/accountmanagement/UserEmail.java
@@ -0,0 +1,46 @@
+package com.flipkart.model.accountmanagement;
+
+public class UserEmail {
+	
+	private String newEmailAddress;
+	private String oldPassword;
+	private String newPassword;
+	private String retypedPassword;
+	
+	
+
+	public String getNewEmailAddress() {
+		return newEmailAddress;
+	}
+
+	public void setNewEmailAddress(String newEmailAddress) {
+		this.newEmailAddress = newEmailAddress;
+	}
+
+	public String getOldPassword() {
+		return oldPassword;
+	}
+
+	public void setOldPassword(String oldPassword) {
+		this.oldPassword = oldPassword;
+	}
+
+	public String getNewPassword() {
+		return newPassword;
+	}
+
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+
+	public String getRetypedPassword() {
+		return retypedPassword;
+	}
+
+	public void setRetypedPassword(String retypedPassword) {
+		this.retypedPassword = retypedPassword;
+	}
+	
+	
+
+}
