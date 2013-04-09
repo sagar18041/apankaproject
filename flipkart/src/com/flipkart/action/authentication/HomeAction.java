8e219ba29c60dd9085ec7e93104cd59c793bae63
rahul.vit09@gmail.com <rahul.vit09@gmail.com@9734beb7-efc3-7ff8-bd58-a83438845b4c>
2013-04-09 19:18:24 +0000

    change in home action to add recommendation

--- flipkart/src/com/flipkart/action/authentication/HomeAction.java
+++ flipkart/src/com/flipkart/action/authentication/HomeAction.java
@@ -7,26 +7,52 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.ServletActionContext;
+
 import com.flipkart.model.authentication.Catalogue;
 import com.flipkart.model.authentication.HomeModel;
 import com.flipkart.model.authentication.SearchList;
 import com.flipkart.model.authentication.SearchListModel;
+import com.flipkart.model.browseitem.ProductModel;
 import com.flipkart.model.cartmanagement.Cart;
+import com.flipkart.model.recommendation.RecentlyViewed;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
  * @author Team404
- *
+ * 
  */
 public class HomeAction extends ActionSupport {
-	
-	private ArrayList<String> categoryList  = new ArrayList<String>();
+
+	private ArrayList<String> categoryList = new ArrayList<String>();
 	private ArrayList<SearchList> sl = new ArrayList<SearchList>();
 	private ArrayList<Catalogue> catalogueList = new ArrayList<Catalogue>();
-	ArrayList<Cart>cartItems=new ArrayList<Cart>();
-	Map cartSession=ActionContext.getContext().getSession();
-	
+	ArrayList<Cart> cartItems = new ArrayList<Cart>();
+	Map cartSession = ActionContext.getContext().getSession();
+	private ArrayList<RecentlyViewed> recentlyViewedItems = new ArrayList<RecentlyViewed>();
+	private ArrayList<RecentlyViewed> browsingHistoryItems = new ArrayList<RecentlyViewed>();
+
+	public ArrayList<RecentlyViewed> getRecentlyViewedItems() {
+		return recentlyViewedItems;
+	}
+
+	public void setRecentlyViewedItems(
+			ArrayList<RecentlyViewed> recentlyViewedItems) {
+		this.recentlyViewedItems = recentlyViewedItems;
+	}
+
+	public ArrayList<RecentlyViewed> getBrowsingHistoryItems() {
+		return browsingHistoryItems;
+	}
+
+	public void setBrowsingHistoryItems(
+			ArrayList<RecentlyViewed> browsingHistoryItems) {
+		this.browsingHistoryItems = browsingHistoryItems;
+	}
+
 	String searchBy;
 	String autoCompleteList;
 	public String categorySel;
@@ -96,6 +122,7 @@ public class HomeAction extends ActionSupport {
 	public void setInitialCartCount(int initialCartCount) {
 		this.initialCartCount = initialCartCount;
 	}
+
 	public Map getCartSession() {
 		return cartSession;
 	}
@@ -106,20 +133,20 @@ public class HomeAction extends ActionSupport {
 
 	public String execute() throws SQLException {
 
-		//get all categories from category table to populate the search box
+		// get all categories from category table to populate the search box
 		System.out.println("Category is " + getCategorySel());
 		HomeModel hm = new HomeModel();
 		categoryList = hm.getCategoryList();
 
-		if(loadFirstTime==0){
-			cartSession.put("cartItems",cartItems);
+		if (loadFirstTime == 0) {
+			cartSession.put("cartItems", cartItems);
 			cartSession.put("cartCount", initialCartCount);
-			loadFirstTime=1;
-			
-			System.out.println("********"+cartSession.get("cartCount"));
+			loadFirstTime = 1;
+
+			System.out.println("********" + cartSession.get("cartCount"));
 		}
 
-		//get all catalogue details to populate menu list
+		// get all catalogue details to populate menu list
 		catalogueList = hm.getCatalogueList();
 
 		/*
@@ -128,10 +155,46 @@ public class HomeAction extends ActionSupport {
 		sl = SearchListModel.fetchList(getCategorySel());
 		System.out.println("SearchList size " + sl.size());
 		autoCompleteList = "[\"";
-		for(int i = 0 ; i < sl.size() ; i++){
-			autoCompleteList = autoCompleteList + sl.get(i).fieldValue + "\",\"";
+		for (int i = 0; i < sl.size(); i++) {
+			autoCompleteList = autoCompleteList + sl.get(i).fieldValue
+					+ "\",\"";
 		}
 		autoCompleteList = autoCompleteList + "\"]";
+
+		// log user browsing history into DB for recommendations
+		HttpServletRequest request = ServletActionContext.getRequest();
+		// to get actual IP not proxy address
+		String ipAddr = request.getHeader("X-FORWARDED-FOR");
+		// System.out.println(ipAddr);
+		// if we use localhost we will get null value so to avoid null convert
+		// it to ip
+		if (ipAddr == null || ipAddr.equals("")) {
+			ipAddr = "127.0.0.1";
+		}
+		// for recommendation
+		ProductModel pm = new ProductModel();
+		// get 5 recently viewed itemIDs
+		ArrayList<Integer> itemIDsForRecentlyViewedItems = new ArrayList<Integer>();
+		itemIDsForRecentlyViewedItems = pm.getRecentlyViewedItems(ipAddr);
+		// System.out.println(itemIDsForRecentlyViewedItems.size());
+
+		// get item details
+		recentlyViewedItems = pm
+				.getRecentlyViewedItems(itemIDsForRecentlyViewedItems);
+
+		/*
+		 * for (int i=0; i<recentlyViewedItems.size();i++) {
+		 * System.out.println(recentlyViewedItems.get(i).getItemName()); }
+		 */
+
+		// System.out.println(recentlyViewedItems.size());
+		// get 5 items based on browsing history
+		ArrayList<Integer> itemIDsForBrowsingHistory = new ArrayList<Integer>();
+		itemIDsForBrowsingHistory = pm.getbrowsedItems(ipAddr);
+
+		// get item details
+		browsingHistoryItems = pm
+				.getRecentlyViewedItems(itemIDsForBrowsingHistory);
 
 		return SUCCESS;
 	}
