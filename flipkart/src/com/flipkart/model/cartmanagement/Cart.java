2845c235e41a49a6ca65c6c390fd1b129f29cb52
avipsa.nayak@gmail.com <avipsa.nayak@gmail.com@9734beb7-efc3-7ff8-bd58-a83438845b4c>
2013-04-09 17:04:16 +0000

    +Ritu- cart changes

--- flipkart/src/com/flipkart/model/cartmanagement/Cart.java
+++ flipkart/src/com/flipkart/model/cartmanagement/Cart.java
@@ -1,70 +1,77 @@
 package com.flipkart.model.cartmanagement;
 
 public class Cart {
-	private String itemDescription;
-	private String deliveryTime;
-	private int itemID;
-	private float price;
-	private int quantity;
-	private float subTotal;
-	private String imgURL;
-	private int status2;
-	private int statusQuantity;
-	
-	public String getImgURL() {
-		return imgURL;
-	}
-	public void setImgURL(String imgURL) {
-		this.imgURL = imgURL;
-	}
-	public int getItemID() {
-		return itemID;
-	}
-	public void setItemID(int itemID) {
-		this.itemID = itemID;
-	}
-	public String getItemDescription() {
-		return itemDescription;
-	}
-	public void setItemDescription(String itemDescription) {
-		this.itemDescription = itemDescription;
-	}
-	public String getDeliveryTime() {
-		return deliveryTime;
-	}
-	public void setDeliveryTime(String deliveryTime) {
-		this.deliveryTime = deliveryTime;
-	}
-	public float getPrice() {
-		return price;
-	}
-	public void setPrice(float price) {
-		this.price = price;
-	}
-	public int getQuantity() {
-		return quantity;
-	}
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-	public float getSubTotal() {
-		return subTotal;
-	}
-	public void setSubTotal(float subTotal) {
-		this.subTotal = subTotal;
-	}
-	public int getStatus2() {
-		return status2;
-	}
-	public void setStatus2(int status2) {
-		this.status2 = status2;
-	}
-	public int getStatusQuantity() {
-		return statusQuantity;
-	}
-	public void setStatusQuantity(int statusQuantity) {
-		this.statusQuantity = statusQuantity;
-	}
-	
-	
+        private String itemDescription;
+        private String deliveryTime;
+        private String subCategory;
+        private int itemID;
+        private float price;
+        private int quantity;
+        private float subTotal;
+        private String thumbnail;
+        private int status2;
+        private int statusQuantity;
+        
+		public String getSubCategory() {
+			return subCategory;
+		}
+		public void setSubCategory(String subCategory) {
+			this.subCategory = subCategory;
+		}
+        public String getThumbnail() {
+			return thumbnail;
+		}
+		public void setThumbnail(String thumbnail) {
+			this.thumbnail = thumbnail;
+		}
+		public int getItemID() {
+                return itemID;
+        }
+        public void setItemID(int itemID) {
+                this.itemID = itemID;
+        }
+        public String getItemDescription() {
+                return itemDescription;
+        }
+        public void setItemDescription(String itemDescription) {
+                this.itemDescription = itemDescription;
+        }
+        public String getDeliveryTime() {
+                return deliveryTime;
+        }
+        public void setDeliveryTime(String deliveryTime) {
+                this.deliveryTime = deliveryTime;
+        }
+        public float getPrice() {
+                return price;
+        }
+        public void setPrice(float price) {
+                this.price = price;
+        }
+        public int getQuantity() {
+                return quantity;
+        }
+        public void setQuantity(int quantity) {
+                this.quantity = quantity;
+        }
+        public float getSubTotal() {
+                return subTotal;
+        }
+        public void setSubTotal(float subTotal) {
+                this.subTotal = subTotal;
+        }
+        public int getStatus2() {
+                return status2;
+        }
+        public void setStatus2(int status2) {
+                this.status2 = status2;
+        }
+        public int getStatusQuantity() {
+                return statusQuantity;
+        }
+        public void setStatusQuantity(int statusQuantity) {
+                this.statusQuantity = statusQuantity;
+        }
+        
+        
 }
