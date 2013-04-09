fcf0ba5975dcc6e0ca57ff767c12792542cf33ac
rahul.vit09@gmail.com <rahul.vit09@gmail.com@9734beb7-efc3-7ff8-bd58-a83438845b4c>
2013-04-09 17:27:13 +0000

    +Anand product, Rating , Review

--- flipkart/src/com/flipkart/action/browseitem/ProductAction.java
+++ flipkart/src/com/flipkart/action/browseitem/ProductAction.java
@@ -7,10 +7,16 @@ package com.flipkart.action.browseitem;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.ServletActionContext;
+
 import com.flipkart.model.browseitem.Attributes;
 import com.flipkart.model.browseitem.Product;
 import com.flipkart.model.browseitem.ProductModel;
+import com.flipkart.model.browseitem.Rating;
 import com.flipkart.model.browseitem.Review;
+import com.flipkart.model.recommendation.RecentlyViewed;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class ProductAction extends ActionSupport {
@@ -19,8 +25,44 @@ public class ProductAction extends ActionSupport {
 	private Product prod = new Product();
 	private ArrayList<Attributes> attrib = new ArrayList<Attributes>();
 	private ArrayList<Review> review = new ArrayList<Review>();
-	
-	
+	private ArrayList<Rating> rating = new ArrayList<Rating>();
+	private Integer finalRating = 0;
+	private ArrayList<RecentlyViewed> recentlyViewedItems = new ArrayList<RecentlyViewed>();
+	private ArrayList<RecentlyViewed> browsingHistoryItems = new ArrayList<RecentlyViewed>();
+	public ArrayList<RecentlyViewed> getBrowsingHistoryItems() {
+		return browsingHistoryItems;
+	}
+
+	public void setBrowsingHistoryItems(
+			ArrayList<RecentlyViewed> browsingHistoryItems) {
+		this.browsingHistoryItems = browsingHistoryItems;
+	}
+
+	public ArrayList<RecentlyViewed> getRecentlyViewedItems() {
+		return recentlyViewedItems;
+	}
+
+	public void setRecentlyViewedItems(ArrayList<RecentlyViewed> recentlyViewedItems) {
+		this.recentlyViewedItems = recentlyViewedItems;
+	}
+
+	public Integer getFinalRating() {
+		return finalRating;
+	}
+
+	public void setFinalRating(Integer finalRating) {
+		this.finalRating = finalRating;
+	}
+
+	public ArrayList<Rating> getRating() {
+		return rating;
+	}
+
+	public void setRating(ArrayList<Rating> rating) {
+		this.rating = rating;
+	}
+
+
 	public ArrayList<Review> getReview() {
 		return review;
 	}
@@ -30,17 +72,18 @@ public class ProductAction extends ActionSupport {
 		this.review = review;
 	}
 
-
-	/** function to get called when a particular item is being clicked
+	/** 
+	 * function to get called when a particular item is being clicked
 	 * @return item details
 	 * @throws SQLException
 	 */
 	public String displayItem() throws SQLException {
 		
 		ProductModel pm = new ProductModel();
-		
-		//get product
+
+		//get product details
 		prod = pm.getProductDetails(itemID);
+		
 		//get product attributes
 		attrib = pm.getProductAttributes(itemID);
 		
@@ -51,18 +94,57 @@ public class ProductAction extends ActionSupport {
 		//get product review
 		review = pm.getProductReview(ProductID);
 		
-		for (int i=0; i<review.size();i++ ) {
-			System.out.println(review.get(i).getReviewTitle());
-			System.out.println(review.get(i).getReviewText());
+		//get product ratings 
+		rating = pm.getProductRating(ProductID);
+		
+		//calculate final rating
+		for (int i = 0; i < rating.size(); i++) {
+			finalRating = finalRating + rating.get(i).getRatingStar();
+		}
+		if(rating.size() > 0) {
+		finalRating = finalRating / rating.size();
 		}
 		
+		//log user browsing history into DB for recommendations
+		HttpServletRequest request = ServletActionContext.getRequest();
+		//to get actual IP not proxy address
+		String ipAddr = request.getHeader("X-FORWARDED-FOR");
+		//System.out.println(ipAddr);
+		//if we use localhost we will get null value so to avoid null convert it to ip
+		if (ipAddr.equals("null")) {
+			ipAddr = "127.0.0.1";
+		}
+		//store into DB
+		pm.putBrowingHistory(ipAddr, itemID);
+		
+		//get 5 recently viewed itemIDs
+		ArrayList<Integer> itemIDsForRecentlyViewedItems = new ArrayList<Integer>();
+		itemIDsForRecentlyViewedItems = pm.getRecentlyViewedItems(ipAddr);
+		//System.out.println(itemIDsForRecentlyViewedItems.size());
+		
+		//get item details
+		recentlyViewedItems = pm.getRecentlyViewedItems(itemIDsForRecentlyViewedItems);
+		
+		/*for (int i=0; i<recentlyViewedItems.size();i++) {
+			System.out.println(recentlyViewedItems.get(i).getItemName());
+		}*/
+		
+		//System.out.println(recentlyViewedItems.size());
+		//get 5 items based on browsing history
+		ArrayList<Integer> itemIDsForBrowsingHistory = new ArrayList<Integer>();
+		itemIDsForBrowsingHistory = pm.getbrowsedItems(ipAddr);
+		
+		//get item details
+		browsingHistoryItems = pm.getRecentlyViewedItems(itemIDsForBrowsingHistory);
+		
 		/*
+		 * 
 		 to be implemented later
-		 pm.getProductRating(itemID)
-		 pm.getProductImages(itemID)
-		 pm.getProductOffers(itemID)
-		 pm.getProductVariants(itemID)
-		 
+		 pm.getProductImages(itemID) 
+		 pm.getProductOffers(itemID) //special case to be implemented at the end
+		 pm.getProductVariants(itemID) //
+		 stock detail
+		 *
 		 */
 		
 		return SUCCESS;
---
+++ flipkart/src/com/flipkart/model/recommendation/BrowseHistory.java
@@ -0,0 +1,5 @@
+package com.flipkart.model.recommendation;
+
+public class BrowseHistory {
+
+}
