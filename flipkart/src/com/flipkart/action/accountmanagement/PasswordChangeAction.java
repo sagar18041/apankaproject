cceb7eb2296a6156baebbf178a42931f21788a22
dipanzan.ddb <dipanzan.ddb@9734beb7-efc3-7ff8-bd58-a83438845b4c>
2013-04-15 23:13:45 +0000

    added action errors...

--- flipkart/src/com/flipkart/action/accountmanagement/PasswordChangeAction.java
+++ flipkart/src/com/flipkart/action/accountmanagement/PasswordChangeAction.java
@@ -1,7 +1,9 @@
 package com.flipkart.action.accountmanagement;
 
+import java.sql.SQLException;
 import java.util.Map;
 
+import com.flipkart.action.authentication.HomeAction;
 import com.flipkart.model.accountmanagement.PersonalInformationModel;
 import com.flipkart.model.accountmanagement.UserPassword;
 import com.flipkart.model.accountmanagement.UserPasswordModel;
@@ -16,6 +18,9 @@ public class PasswordChangeAction extends ActionSupport {
 	private String oldPassword;
 	private String newPassword;
 	private String retypedPassword;
+	private String errorMsg;
+	private String actionMsg;
+	
 	Map sess=ActionContext.getContext().getSession();
 	
 	public int getUserID() {
@@ -57,6 +62,21 @@ public class PasswordChangeAction extends ActionSupport {
 	public void setRetypedPassword(String retypedPassword) {
 		this.retypedPassword = retypedPassword;
 	}
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
+
+	public String getActionMsg() {
+		return actionMsg;
+	}
+
+	public void setActionMsg(String actionMsg) {
+		this.actionMsg = actionMsg;
+	}
 
 	public String loadPage(){
 		setUserID(Integer.valueOf(sess.get("userID").toString()));
@@ -65,11 +85,12 @@ public class PasswordChangeAction extends ActionSupport {
 	}
 
 	public String modifyPassword(){
-
+		
 		setUserID(Integer.valueOf(sess.get("userID").toString()));
 		setEmailAddress(UserPasswordModel.getUserEmailAddress(userID));
 		if(oldPassword.equals("") || newPassword.equals("") || retypedPassword.equals("")){
-			addActionError("Password cannot be left empty.");
+			errorMsg="Password cannot be left empty.";
+			//addActionError("");
 			return ERROR;
 		}
 		else{
@@ -86,21 +107,25 @@ public class PasswordChangeAction extends ActionSupport {
 				if(up.getNewPassword().equalsIgnoreCase(up.getRetypedPassword())){
 					int result = UserPasswordModel.updateUserPassword(up.getNewPassword(), userID);
 					if(result!=0){
-						addActionMessage("Your changes have been saved successfully.");
+						actionMsg = "Your changes have been saved successfully.";
+						//addActionMessage("Your changes have been saved successfully.");
 						return SUCCESS;
 					}
 					else{
-						addActionError("Some error has occured. Please try again.");
+						errorMsg = "Some error has occured. Please try again.";
+						//addActionError("Some error has occured. Please try again.");
 						return ERROR;
 					}
 				}
 				else{
-					addActionError("Password change failed. New Passwords do not match.");
+					errorMsg = "Password change failed. New Passwords do not match.";
+					//addActionError("");
 					return ERROR;
 				}
 			}
 			else{
-				addActionError("Email/Password combination is wrong.");
+				errorMsg = "Email/Password combination is wrong.";
+				//addActionError("");
 				return ERROR;
 			}
 		}
