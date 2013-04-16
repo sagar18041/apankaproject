567a63aec2ec37cf2e7240320920209531fd479b
dipanzan.ddb <dipanzan.ddb@9734beb7-efc3-7ff8-bd58-a83438845b4c>
2013-04-16 00:07:25 +0000

    added action errors...

--- flipkart/src/com/flipkart/model/cartmanagement/CartModel.java
+++ flipkart/src/com/flipkart/model/cartmanagement/CartModel.java
@@ -1,5 +1,31 @@
 package com.flipkart.model.cartmanagement;
 
-public class CartModel {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import com.flipkart.util.DbConnection;
+
+public class CartModel {
+	static PreparedStatement ps = null;
+	static ResultSet rs = null;
+	static String sqlQuery = "";
+	static Connection conn = null;
+	
+	public static int checkAvailableQuantity(int itemID){
+		sqlQuery = "select availableQuantity from flipkart_item where itemID=?;";
+		try{
+			conn=DbConnection.getConnection();
+			ps=conn.prepareStatement(sqlQuery);
+			ps.setInt(1, itemID);
+			rs=ps.executeQuery();
+
+			if(rs.next()){
+				return rs.getInt(1);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return 0;
+	}
 }
