3ca29096ddf15df609139feef448eeb6253ca028
suvadratripathy88@gmail.com <suvadratripathy88@gmail.com@9734beb7-efc3-7ff8-bd58-a83438845b4c>
2013-04-22 10:15:40 +0000

    final changes to stock mgmt done. :)

--- trunk/flipkart/src/com/flipkart/action/stockmanagement/StockmanagementAction.java
+++ trunk/flipkart/src/com/flipkart/action/stockmanagement/StockmanagementAction.java
@@ -3,9 +3,6 @@ package com.flipkart.action.stockmanagement;
 import java.util.ArrayList;
 
 import javax.mail.MessagingException;
-
-import com.flipkart.model.authentication.SignUpModel;
-import com.flipkart.model.payment.MakePaymentModel;
 import com.flipkart.model.stockmanagement.Stockmanagement;
 import com.flipkart.model.stockmanagement.StockmanagementModel;
 import com.flipkart.util.EmailManager;
@@ -16,10 +13,11 @@ public class StockmanagementAction extends ActionSupport{
 
 	private int itemID;
 	private String itemNameUpdated;
-	private int newQuantity;
+	private String newQuantity;
+	private String errMsg;
 
 	String[] checkItems;
-	int checkedSeller;
+	int checkedSeller=-1;
 	int sellerIDFromMail=-1;
 
 	ArrayList<Stockmanagement> allItems=new ArrayList<Stockmanagement>();
@@ -30,50 +28,54 @@ public class StockmanagementAction extends ActionSupport{
 
 		itemsBelowThreshold = StockmanagementModel.getItemsBelowThreshold();
 		sellers = StockmanagementModel.getSellerInfo();
-
+		setCheckedSeller(-1);
+		setCheckItems(null);
 		return SUCCESS;
 	}
 
 	public String orderItemStock(){
 
-		System.out.println("Seller:"+checkedSeller);
-		System.out.println("checkItems.len"+checkItems.length);
-
-		ArrayList<Stockmanagement> allCheckedItems = new ArrayList<Stockmanagement>(); 
-
-		for(int i=0; i<checkItems.length; i++ ){
-
-			//get the selected itemID and then get its itemDetails
-			int tempItemID = Integer.parseInt(checkItems[i].toString());
-
-			/*
-			 * ThresholdFlag = 0 means NO REORDER REQUESTED
-			 * ThresholdFlag = 1 means REORDER REQUESTED
-			 */
-			StockmanagementModel.SetThresholdFlag1(tempItemID);
-			
-			//get the item object
-			Stockmanagement tempObj = StockmanagementModel.getItemInfo(tempItemID);
-
-			//add the new object to arraylist
-			allCheckedItems.add(tempObj);
-
+		//If all values were not selected
+		if(checkItems==null || checkItems.length==0 || checkedSeller==-1){
+			addActionError("Please select the necessary feilds.");
+			return ERROR;
 		}
+		else{
+			ArrayList<Stockmanagement> allCheckedItems = new ArrayList<Stockmanagement>(); 
 
-		try{
-			//send arraylist and sellerInfo to mail
-			sendReorderEmail(checkedSeller, allCheckedItems);
-			
-		} catch(MessagingException e){
-			
+			for(int i=0; i<checkItems.length; i++ ){
+
+				//get the selected itemID and then get its itemDetails
+				int tempItemID = Integer.parseInt(checkItems[i].toString());
+
+				/*
+				 * ThresholdFlag = 0 means NO REORDER REQUESTED
+				 * ThresholdFlag = 1 means REORDER REQUESTED
+				 */
+				StockmanagementModel.SetThresholdFlag1(tempItemID);
+
+				//get the item object
+				Stockmanagement tempObj = StockmanagementModel.getItemInfo(tempItemID);
+
+				//add the new object to arraylist
+				allCheckedItems.add(tempObj);
+
+			}
+
+			try{
+				//send arraylist and sellerInfo to mail
+				sendReorderEmail(checkedSeller, allCheckedItems);
+
+			} catch(MessagingException e){
+
+			}
 		}
-		
 		return SUCCESS;
 	}
 
 
 	public void sendReorderEmail(int sellerID, ArrayList<Stockmanagement> itemList) throws MessagingException{
-		MakePaymentModel mpm = new MakePaymentModel();
+		//MakePaymentModel mpm = new MakePaymentModel();
 
 		String link = "http://localhost:8080/flipkart/updatequantity?sellerIDFromMail="+sellerID;
 		String emailAdd= "flipkart.team403.seller@gmail.com";
@@ -87,7 +89,7 @@ public class StockmanagementAction extends ActionSupport{
 				+ "<br/><br/>Please find below the summary of the order:"
 				+ "<br/><br/><html><table border='1'><tr><td>ItemName</td><td>Price(Unit Rs.)</td></tr>";
 
-		
+
 		for(int i = 0 ; i < itemList.size() ; i++){
 			messageBody = messageBody +
 					"<tr>" +
@@ -97,7 +99,7 @@ public class StockmanagementAction extends ActionSupport{
 		}
 		messageBody = messageBody	+ "</table></html>";
 		messageBody = messageBody + "<br/><br/>Click on the following link to refurnish the stock:<br/><br/>" 
-								  + link + "<br/><br/>...............................................................................................................";
+				+ link + "<br/><br/>...............................................................................................................";
 		messageBody = messageBody	+ "<br/><br/>Please contact us should you have any questions or queries regarding the order."
 				+ "<br/><br/><br/>Thank you for your continuous partnership and support !"
 				+ "<br/><br/>..............................................................................................................."
@@ -116,10 +118,13 @@ public class StockmanagementAction extends ActionSupport{
 
 	public String viewItems(){
 
+		System.out.println("in stock mgmt action :::"+errMsg);
+		if(errMsg != null){
+			addActionError(errMsg);
+		}
 		allItems.clear();
 		allItems=StockmanagementModel.fetchAllItems();
 		System.out.println("size.."+allItems.size());
-
 		for(int i=0;i<allItems.size();i++)
 			System.out.println("price.."+allItems.get(i).getPrice());
 		return SUCCESS;
@@ -127,10 +132,24 @@ public class StockmanagementAction extends ActionSupport{
 
 	public String updateQuantityInStock(){
 
-		System.out.println(" ");
-		StockmanagementModel.updateQuantityInStock(itemID,newQuantity);
-		StockmanagementModel.insertTransaction(itemID,StockmanagementModel.getItemPrice(itemID),getNewQuantity());
+		System.out.println("in update qty:::"+itemID+"..."+newQuantity);
+		try{
+			int checkInt = Integer.parseInt(newQuantity);
+		}
+		catch(Exception e){
+			System.out.println("in excep..");
+			errMsg="Enter a valid quantity";
+			return SUCCESS;
+		}
+		if(Integer.parseInt(newQuantity)<=0){
+			System.out.println("in if...");
+			errMsg="Enter a valid quantity";
+			return SUCCESS;
+		}
+		StockmanagementModel.updateQuantityInStock(itemID,Integer.parseInt(newQuantity));
+		StockmanagementModel.insertTransaction(itemID,StockmanagementModel.getItemPrice(itemID),Integer.parseInt(newQuantity));
 		return SUCCESS;
+
 	}
 
 	/*
@@ -143,12 +162,7 @@ public class StockmanagementAction extends ActionSupport{
 		this.itemID = itemID;
 	}
 
-	public int getNewQuantity() {
-		return newQuantity;
-	}
-	public void setNewQuantity(int newQuantity) {
-		this.newQuantity = newQuantity;
-	}
+
 	public ArrayList<Stockmanagement> getAllItems() {
 		return allItems;
 	}
@@ -202,6 +216,22 @@ public class StockmanagementAction extends ActionSupport{
 
 	public void setSellerIDFromMail(int sellerIDFromMail) {
 		this.sellerIDFromMail = sellerIDFromMail;
+	}
+
+	public String getNewQuantity() {
+		return newQuantity;
+	}
+
+	public void setNewQuantity(String newQuantity) {
+		this.newQuantity = newQuantity;
+	}
+
+	public String getErrMsg() {
+		return errMsg;
+	}
+
+	public void setErrMsg(String errMsg) {
+		this.errMsg = errMsg;
 	}
 
 }
